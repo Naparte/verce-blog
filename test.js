f78aba93c4ecb184dd938600a6da41a8c8a71ffa
Naparte <you@example.com>
2023-12-11 20:37:08 +0800

    feat: add post about prettier && playwright functionality adn usage demo

--- test.js
+++ test.js
@@ -1,52 +1,14 @@
-// class MyInfo extends HTMLElement {
-//   constructor() {
-//     super();
+import { chromium } from "playwright";
 
-//     var image = document.createElement("img");
-//     image.src =
-//       "https://github.com/Naparte/verce-blog/blob/main/avatar.png?raw=true";
-//     image.width = 40;
-//     image.classList.add("image");
+(async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-//     var container = document.createElement("div");
-//     container.classList.add("container");
+  await page.goto("https://www.baidu.com");
+  await page.screenshot({ path: "example.png" });
 
-//     var name = document.createElement("p");
-//     name.classList.add("name");
-//     name.innerText = "Name: Naparte";
+  await browser.close();
+})();
 
-//     var email = document.createElement("p");
-//     email.classList.add("email");
-//     email.innerText = "Email: Naparte@163.com";
-
-//     var button = document.createElement("button");
-//     button.classList.add("button");
-//     button.innerText = "Follow";
-
-//     container.append(name, email, button);
-//     this.append(image, container);
-//   }
-// }
-// window.customElements.define("my-info", MyInfo);
-
-class MyInfo extends HTMLElement {
-  constructor() {
-    super();
-
-    var templateElem = document.getElementById("my-info-template");
-    var content = templateElem.content.cloneNode(true);
-
-    //  :host CSS 伪类选择内部使用了该 CSS 的影子 DOM（shadow DOM） 的影子宿主（shadow host）——换句话说，这允许你从其影子 DOM 内部选择自定义元素
-    // https://developer.mozilla.org/zh-CN/docs/Web/CSS/:host
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.appendChild(content);
-
-    // 添加操作事件
-    this.$button = shadowRoot.querySelector("button");
-    this.$button.addEventListener("click", () => {
-      alert("click");
-    });
-  }
-}
-
-window.customElements.define("my-info", MyInfo);
+console.log('ssss')
