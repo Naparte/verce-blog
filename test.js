eac41063a40a570fb0251bbf936bbaacdd73a61d
Naparte <you@example.com>
2023-12-13 14:21:37 +0800

    feat: add the you do not know about javascript template

--- test.js
+++ test.js
@@ -1,16 +1,40 @@
-import mm from 'micromatch';
-// mm.all(string, patterns[, options]);
+const data = {
+  name: "Naparte",
+  sex: 0,
+  role: 1,
+  time: new Date().getTime(),
+};
+const invite = function (arrs, nameExp, sexExp, roleExp, timeExp) {
+  let strName = nameExp;
+  // 性别处理
+  let strSex = ["先生", "女士"][sexExp];
+  // 角色处理
+  const role = {
+    1: "微前端",
+    2: "webpack",
+    3: "可视化",
+    4: "工程化",
+  };
+  let strRole = role[roleExp];
+  // 日期处理
+  let strTime = new Date(timeExp).toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
-console.log(mm.all('foo.js', ['foo.js']));
-// true
+  // 输出内容
+  let output = [arrs[0]];
 
-console.log(mm.all('foo.js', ['*.js', '!foo.js']));
-// false
+  [strName, strSex, strRole, strTime].forEach((str, index) => {
+    output.push(str, arrs[index + 1] || "");
+  });
 
-console.log(mm.all('foo.js', ['*.js', 'foo.js']));
-// true
+  return output.join("");
+};
 
-console.log(mm.all('foo.js', ['*.js', 'f*', '*o*', '*o.js']));
-// true
+let content = invite`诚挚邀 ${data.name}${data.sex}，作为 ${data.role} 领域优秀开发，参加${data.time}度FrontEnd前端技术大会。本次大会旨在为广大前端开发者提供一个交流、学习和展示的平台，共同探讨前端技术的最新趋势和发展方向。主办方：深圳市前端开发者协会`;
 
-// (16) ['match', 'matcher', 'isMatch', 'any', 'not', 'contains', 'matchKeys', 'some', 'every', 'all', 'capture', 'makeRe', 'scan', 'parse', 'braces', 'braceExpand']
+console.log(content);
+// 诚挚邀 Naparte先生，作为 微前端 领域优秀开发，参加2023年12月13日度FrontEnd前端技术大会。本次大会旨在为广大前端开发者提供一个交流、学习和展示的平台，共同探讨前端技术的最新趋势和发展方向。主办方：深圳市前端
+// 开发者协会;
