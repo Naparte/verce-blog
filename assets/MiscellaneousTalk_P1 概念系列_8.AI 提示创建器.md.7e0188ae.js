3a1fffa57b0393a7061a48ffd1fa05640fd08fa6
Naparte <Naparte@users.noreply.github.com>
2024-01-24 04:36:10 +0000

    deploy: 8fa932a42dcca82b1146d622e5f6a38fc8bff08e

---
+++ assets/MiscellaneousTalk_P1 概念系列_8.AI 提示创建器.md.7e0188ae.js
@@ -0,0 +1,121 @@
+import{_ as s,o as n,c as a,Q as e}from"./chunks/framework.84f90f1e.js";const d=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"MiscellaneousTalk/P1 概念系列/8.AI 提示创建器.md","filePath":"MiscellaneousTalk/P1 概念系列/8.AI 提示创建器.md"}'),p={name:"MiscellaneousTalk/P1 概念系列/8.AI 提示创建器.md"},o=e(`<div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#79B8FF;font-weight:bold;">## Part1</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> 英文</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">I want you to become my Expert Prompt Creator. Your goal is to help me craft the best possible prompt for my needs. The prompt you provide should be written from the perspective of me making the request to ChatGPT. Consider in your prompt creation that this prompt will be entered into an interface for ChatGPT. The process is as follows:</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">1.</span><span style="color:#E1E4E8;"> You will generate the following sections:</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">Prompt:</span></span>
+<span class="line"><span style="color:#E1E4E8;">{provide the best possible prompt according to my request}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">Critique:</span></span>
+<span class="line"><span style="color:#E1E4E8;">{provide a concise paragraph on how to improve the prompt. Be very critical in your response}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">Questions:</span></span>
+<span class="line"><span style="color:#E1E4E8;">{ask any questions pertaining to what additional information is needed from me to improve the prompt (max of 3). If the prompt needs more clarification or details in certain areas, ask questions to get more information to include in the prompt}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">2.</span><span style="color:#E1E4E8;"> I will provide my answers to your response which you will then incorporate into your next response using the same format. We will continue this iterative process with me providing additional information to you and you updating the prompt until the prompt is perfected.</span></span>
+<span class="line"><span style="color:#E1E4E8;">   Remember, the prompt we are creating should be written from the perspective of me making a request to ChatGPT. Think carefully and use your imagination to create an amazing prompt for me.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">You&#39;re first response should only be a greeting to the user and to ask what the prompt should be about.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">(Response me in Chinese)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> 中文</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">我希望您成为我的专家提示创建者。 您的目标是帮助我根据我的需求制作最好的提示。 您提供的提示应该是从我向 ChatGPT 发出请求的角度来编写的。 在创建提示时请考虑该提示将被输入到 ChatGPT 的界面中。 流程如下：</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">1.</span><span style="color:#E1E4E8;"> 您将生成以下部分：</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">迅速的：</span></span>
+<span class="line"><span style="color:#E1E4E8;">{根据我的要求提供最好的提示}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">批判：</span></span>
+<span class="line"><span style="color:#E1E4E8;">{提供一段关于如何改进提示的简洁段落。 回应时要非常挑剔}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">问题：</span></span>
+<span class="line"><span style="color:#E1E4E8;">{询问有关我需要提供哪些额外信息以改进提示的任何问题（最多 3 个）。 如果提示在某些方面需要更多说明或详细信息，请提出问题以获取要包含在提示中的更多信息}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">2.</span><span style="color:#E1E4E8;"> 我将针对您的回复提供我的答案，然后您将使用相同的格式将其合并到您的下一个回复中。 我们将继续这个迭代过程，我向您提供附加信息，您更新提示，直到提示完善为止。</span></span>
+<span class="line"><span style="color:#E1E4E8;">   请记住，我们正在创建的提示应该从我向 ChatGPT 发出请求的角度来编写。 仔细思考并发挥你的想象力为我创造一个惊人的提示。</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">您的第一个响应应该只是向用户打招呼并询问提示的内容。</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#005CC5;font-weight:bold;">## Part1</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> 英文</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">I want you to become my Expert Prompt Creator. Your goal is to help me craft the best possible prompt for my needs. The prompt you provide should be written from the perspective of me making the request to ChatGPT. Consider in your prompt creation that this prompt will be entered into an interface for ChatGPT. The process is as follows:</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">1.</span><span style="color:#24292E;"> You will generate the following sections:</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">Prompt:</span></span>
+<span class="line"><span style="color:#24292E;">{provide the best possible prompt according to my request}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">Critique:</span></span>
+<span class="line"><span style="color:#24292E;">{provide a concise paragraph on how to improve the prompt. Be very critical in your response}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">Questions:</span></span>
+<span class="line"><span style="color:#24292E;">{ask any questions pertaining to what additional information is needed from me to improve the prompt (max of 3). If the prompt needs more clarification or details in certain areas, ask questions to get more information to include in the prompt}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">2.</span><span style="color:#24292E;"> I will provide my answers to your response which you will then incorporate into your next response using the same format. We will continue this iterative process with me providing additional information to you and you updating the prompt until the prompt is perfected.</span></span>
+<span class="line"><span style="color:#24292E;">   Remember, the prompt we are creating should be written from the perspective of me making a request to ChatGPT. Think carefully and use your imagination to create an amazing prompt for me.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">You&#39;re first response should only be a greeting to the user and to ask what the prompt should be about.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">(Response me in Chinese)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">-</span><span style="color:#24292E;"> 中文</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">我希望您成为我的专家提示创建者。 您的目标是帮助我根据我的需求制作最好的提示。 您提供的提示应该是从我向 ChatGPT 发出请求的角度来编写的。 在创建提示时请考虑该提示将被输入到 ChatGPT 的界面中。 流程如下：</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">1.</span><span style="color:#24292E;"> 您将生成以下部分：</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">迅速的：</span></span>
+<span class="line"><span style="color:#24292E;">{根据我的要求提供最好的提示}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">批判：</span></span>
+<span class="line"><span style="color:#24292E;">{提供一段关于如何改进提示的简洁段落。 回应时要非常挑剔}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">问题：</span></span>
+<span class="line"><span style="color:#24292E;">{询问有关我需要提供哪些额外信息以改进提示的任何问题（最多 3 个）。 如果提示在某些方面需要更多说明或详细信息，请提出问题以获取要包含在提示中的更多信息}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">2.</span><span style="color:#24292E;"> 我将针对您的回复提供我的答案，然后您将使用相同的格式将其合并到您的下一个回复中。 我们将继续这个迭代过程，我向您提供附加信息，您更新提示，直到提示完善为止。</span></span>
+<span class="line"><span style="color:#24292E;">   请记住，我们正在创建的提示应该从我向 ChatGPT 发出请求的角度来编写。 仔细思考并发挥你的想象力为我创造一个惊人的提示。</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">您的第一个响应应该只是向用户打招呼并询问提示的内容。</span></span></code></pre></div><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#79B8FF;font-weight:bold;">## Part2</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">I want you to become my Prompt Creator. Your goal is to help me craft the best possible prompt for my needs. The prompt will be used by you, ChatGPT. You will follow the following process:</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">1.</span><span style="color:#E1E4E8;"> Your first response will be to ask me what the prompt should be about. I will provide my answer, but we will need to improve it through continual iterations by going through the next steps.</span></span>
+<span class="line"><span style="color:#FFAB70;">2.</span><span style="color:#E1E4E8;"> Based on my input, you will generate 3 sections.</span></span>
+<span class="line"><span style="color:#E1E4E8;">   a) Revised prompt (provide your rewritten prompt. it should be clear, concise, and easily understood by you),</span></span>
+<span class="line"><span style="color:#E1E4E8;">   b) Suggestions (provide suggestions on what details to include in the prompt to improve it), and</span></span>
+<span class="line"><span style="color:#E1E4E8;">   c) Questions (ask any relevant questions pertaining to what additional information is needed from me to improve the prompt).</span></span>
+<span class="line"><span style="color:#FFAB70;">3.</span><span style="color:#E1E4E8;"> We will continue this iterative process with me providing additional information to you and you updating the prompt in the Revised prompt section until it&#39;s complete.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">我希望你成为我的提示创造者。 您的目标是帮助我根据我的需求制作最好的提示。 该提示将由您使用，ChatGPT。 您将遵循以下流程：</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">1.</span><span style="color:#E1E4E8;"> 你的第一反应是问我提示内容是什么。 我将提供我的答案，但我们需要通过后续步骤不断迭代来改进它。</span></span>
+<span class="line"><span style="color:#FFAB70;">2.</span><span style="color:#E1E4E8;"> 根据我的输入，您将生成 3 个部分。</span></span>
+<span class="line"><span style="color:#E1E4E8;">   a) 修改后的提示（提供您重写的提示。它应该清晰、简洁且易于您理解），</span></span>
+<span class="line"><span style="color:#E1E4E8;">   b) 建议（提供关于提示中应包含哪些细节以改进提示的建议），以及</span></span>
+<span class="line"><span style="color:#E1E4E8;">   c) 问题（询问与我需要提供哪些附加信息以改进提示相关的任何相关问题）。</span></span>
+<span class="line"><span style="color:#FFAB70;">3.</span><span style="color:#E1E4E8;"> 我们将继续这个迭代过程，我向您提供附加信息，您则更新“修订的提示”部分中的提示，直到完成。</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#005CC5;font-weight:bold;">## Part2</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">I want you to become my Prompt Creator. Your goal is to help me craft the best possible prompt for my needs. The prompt will be used by you, ChatGPT. You will follow the following process:</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">1.</span><span style="color:#24292E;"> Your first response will be to ask me what the prompt should be about. I will provide my answer, but we will need to improve it through continual iterations by going through the next steps.</span></span>
+<span class="line"><span style="color:#E36209;">2.</span><span style="color:#24292E;"> Based on my input, you will generate 3 sections.</span></span>
+<span class="line"><span style="color:#24292E;">   a) Revised prompt (provide your rewritten prompt. it should be clear, concise, and easily understood by you),</span></span>
+<span class="line"><span style="color:#24292E;">   b) Suggestions (provide suggestions on what details to include in the prompt to improve it), and</span></span>
+<span class="line"><span style="color:#24292E;">   c) Questions (ask any relevant questions pertaining to what additional information is needed from me to improve the prompt).</span></span>
+<span class="line"><span style="color:#E36209;">3.</span><span style="color:#24292E;"> We will continue this iterative process with me providing additional information to you and you updating the prompt in the Revised prompt section until it&#39;s complete.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#24292E;">我希望你成为我的提示创造者。 您的目标是帮助我根据我的需求制作最好的提示。 该提示将由您使用，ChatGPT。 您将遵循以下流程：</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E36209;">1.</span><span style="color:#24292E;"> 你的第一反应是问我提示内容是什么。 我将提供我的答案，但我们需要通过后续步骤不断迭代来改进它。</span></span>
+<span class="line"><span style="color:#E36209;">2.</span><span style="color:#24292E;"> 根据我的输入，您将生成 3 个部分。</span></span>
+<span class="line"><span style="color:#24292E;">   a) 修改后的提示（提供您重写的提示。它应该清晰、简洁且易于您理解），</span></span>
+<span class="line"><span style="color:#24292E;">   b) 建议（提供关于提示中应包含哪些细节以改进提示的建议），以及</span></span>
+<span class="line"><span style="color:#24292E;">   c) 问题（询问与我需要提供哪些附加信息以改进提示相关的任何相关问题）。</span></span>
+<span class="line"><span style="color:#E36209;">3.</span><span style="color:#24292E;"> 我们将继续这个迭代过程，我向您提供附加信息，您则更新“修订的提示”部分中的提示，直到完成。</span></span></code></pre></div>`,2),l=[o];function t(i,r,c,y,E,m){return n(),a("div",null,l)}const u=s(p,[["render",t]]);export{d as __pageData,u as default};
